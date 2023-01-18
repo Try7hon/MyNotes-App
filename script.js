f5e9aff14586d436af10fa7288feb3f984965e0c
Dominik <wilk.dom132@gmail.com>
2023-01-18 15:27:44 +0000

    Update JS

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+const addBtn = document.querySelector('.add');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const deleteBtns = document.getElementsByClassName('delete-note');
 
+const deleteAllBtn = document.querySelector('.delete-all');
+
+const noteArea = document.querySelector('.note-area');
+const notePanel = document.querySelector('.note-panel');
+const category = document.querySelector('#category');
+const textarea = document.querySelector('#text');
+const error = document.querySelector('.error');
+
+let selectedValue;
+let cardID = 0;
+
+const openPanel = () => {
+	notePanel.style.display = 'flex';
+};
+
+const closePanel = () => {
+	notePanel.style.display = 'none';
+	error.style.visibility = 'hidden';
+	textarea.value = '';
+	category.selectedIndex = 0;
+};
+
+const addNote = () => {
+	if (textarea.value !== '' && category.selectedIndex !== 0) {
+		createNote();
+		error.style.visibility = 'hidden';
+	} else {
+		error.style.visibility = 'visible';
+	}
+};
+const createNote = () => {
+	const newNote = document.createElement('div');
+	newNote.classList.add('note');
+	newNote.setAttribute('id', cardID);
+
+	newNote.innerHTML = `<div class="note-header">
+        <h3 class="note-title">${selectedValue}</h3>
+        <button class="delete-note"><i class="fas fa-times icon"></i></button>
+    </div>
+    <div class="note-body">
+    ${textarea.value}
+    </div>`;
+
+	noteArea.appendChild(newNote);
+	cardID++;
+
+	closePanel();
+};
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text;
+};
+
+saveBtn.addEventListener('click', addNote);
+addBtn.addEventListener('click', openPanel);
+cancelBtn.addEventListener('click', closePanel);
